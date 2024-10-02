250b0151f2ee6f86d96ce41da447b0a9b8a71de8
hoonti06 <hoonti06@gmail.com>
2024-10-02 23:22:56 +0900

    section 객체 지향 패러다임 - ISP: Interface Segregation Principle - Game 인터페이스 추가 및 구현 (ISP가 필요한 상황)

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,15 @@
+package cleancode.minesweeper.tobe;
+
+public class AnotherGame implements Game {
+
+    @Override
+    public void initialize() {
+        // 필요 없음..
+    }
+
+    @Override
+    public void run() {
+        System.out.println("AnotherGame run");
+    }
+
+}
